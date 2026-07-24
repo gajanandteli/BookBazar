e0d308fcdfe02658264a7924281ff3d0b2ca37f8
gajanandteli <gajanandteli865@gmail.com>
2026-07-24 12:01:52 +0530

    Fix chat api

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL ?? '${process.env.NEXT_PUBLIC_API_URL}',
+  baseURL:
+    process.env.NEXT_PUBLIC_API_URL ||
+    "https://bookbazar-sw7j.onrender.com",
 });
 
 export default api;
