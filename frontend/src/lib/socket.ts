743f09680dc5504b452444b21a4f222260030ef5
gajanandteli <gajanandteli865@gmail.com>
2026-07-24 11:18:25 +0530

    Fix Socket URL

--- frontend/src/lib/socket.ts
+++ frontend/src/lib/socket.ts
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("${process.env.NEXT_PUBLIC_API_URL}", {
+export const socket = io(process.env.NEXT_PUBLIC_API_URL!, {
   autoConnect: true,
+  transports: ["websocket", "polling"],
 });
